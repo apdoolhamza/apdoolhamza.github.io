d2e50182a2e314951676de374a871247d1f0ea4b
Apdoolmajeed Hamza <147955013+apdoolhamza@users.noreply.github.com>
2025-09-23 22:10:56 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -73,7 +73,7 @@ link.classList.add('active');
 // typing-effect-script
 document.addEventListener('DOMContentLoaded', function() {
 const typingText = document.getElementById('typing-text');
-const texts = ['Full-Stack Web Developer', 'AI/ML Engineer', 'UI/UX Designer', 'Freelancer'];
+const texts = ['Full-Stack Web Developer', 'AI/ML Engineer', 'UI/UX Designer'];
 let textIndex = 0;
 let charIndex = 0;
 let isDeleting = false;
